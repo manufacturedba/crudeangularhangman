bc4ea8ccc69d27944e6a695c57f40bad32cc74c8
unknown <rrodriguez@al-rrodriguez.commandalkon.local>
2014-01-13 10:28:06 -0600

    reset function with prompt. idk what else.

--- js/hangman.js
+++ js/hangman.js
@@ -3,23 +3,37 @@ var hangmanAppModule = angular.module('hangmanApp', []);
 hangmanAppModule.controller('hangmanCtrl', function($scope) {
 	this.fails = 0;
 	this.tries = 4;
-	this.word = 'command';
 	this.letterChoice = "";
 	this.lettersUsed = [''];
-	this.discovered = new Array();
 	this.uncovered = '';
+	this.word = "commandalkon";
+	this.discovered = new Array();	
+
+	this.reset = function reset(){
+		this.fails = 0;
+		this.lettersUsed = [''];
+		this.uncovered = '';
+		this.discovered = new Array;
+		}
 	this.tick = function tick (){
 		if (this.letterCheck() == true) {
 			this.lettersUsed.push(this.letterChoice);
 					if (this.word.indexOf(this.letterChoice) == -1) {
 						this.fails = this.fails + 1;
-						if (this.fails >= this.tries) { window.alert("you're dead")}
+						if (this.fails >= this.tries) { 
+							if (confirm("You've LOST\nPlay again?") == true) {
+								this.reset();
+							}
+						}
 					}
 					else {
 						this.letterInsert(this.letterChoice);
 						if (this.uncovered.length == this.word.length) { 
-							window.alert("YOU'VE WON");
+							if (confirm("YOU'VE WON\nPlay again?") == true) {
+								this.reset();
 							}
+						}
+
 					}
 				}
 	}
@@ -39,8 +53,6 @@ hangmanAppModule.controller('hangmanCtrl', function($scope) {
 		while (!(index == -1)) {
 			this.discovered[index] = letter;
 			index = this.word.indexOf(letter, index + 1);
-			window.alert(index);
-			window.alert(this.discovered);
 			this.uncovered = this.discovered.join('');
 		}
 	}
